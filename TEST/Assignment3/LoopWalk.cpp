84a341359b2a13ee3255c637f7bbf952eac9cf03
SIMONE MUSSINI <284900@studenti.unimore.it>
2024-05-18 12:10:51 +0200

    Fixed issue with dead istructions boolean logic. Sligthly optimized code flow. Ordered instructions to move.
    Co-authored-by: paridestomeo <paridestomeo@users.noreply.github.com>

--- TEST/Assignment3/LoopWalk.cpp
+++ TEST/Assignment3/LoopWalk.cpp
@@ -8,7 +8,7 @@ using namespace llvm;
 void printLoopInfo(llvm::Loop &L) {
   // Check if the loop is in simplified form
   if (L.isLoopSimplifyForm()) {
-    outs() << "LoopWalk: Loop is in simplified form\n";
+    outs() << "LOOP INFO:\n";
     
     outs() << "\nLoop pre-header: ";
     L.getLoopPreheader()->print(outs());
@@ -51,16 +51,46 @@ bool isInstructionLoopInvariant(llvm::Instruction *I, Loop &L, std::unordered_se
   return true;
 }
 
+bool checkIfInstructionIsDead(llvm::Instruction *I, Loop &L) {
+  bool isDead = true;
+  for(auto &use : I->uses()) {
+    if (llvm::Instruction *instruction = llvm::dyn_cast<llvm::Instruction>(use)) {
+      isDead &= L.contains(instruction->getParent());
+    } else {
+      return false;
+    }
+  }
+  return isDead;
+}
+
+bool hasSideEffects(llvm::Instruction *I) {
+  switch (I->getOpcode()) {
+    case llvm::Instruction::PHI:
+    case llvm::Instruction::Call:
+    case llvm::Instruction::Br:
+    case llvm::Instruction::Ret:
+    case llvm::Instruction::Load:
+    case llvm::Instruction::Store:
+      return true;
+      break;
+
+    default:
+      return false;
+      break;
+  }
+}
+
 PreservedAnalyses LoopWalk::run(Loop &L, LoopAnalysisManager &AM, LoopStandardAnalysisResults &AR, LPMUpdater &U) {
 
   printLoopInfo(L);
 
   std::unordered_set<llvm::Instruction*> loopInvariantInstructions{};
 
+  outs() << "INSTRUCTIONS: \n";
   // Go through all the instructions in the loop and check if they are loop invariant
   for (auto &BB : L.blocks()) {
     for (llvm::Instruction &I : *BB) {
-      if (isInstructionLoopInvariant(&I, L, loopInvariantInstructions)) {
+      if (isInstructionLoopInvariant(&I, L, loopInvariantInstructions) && !hasSideEffects(&I)) {
         loopInvariantInstructions.insert(&I);
       }
     }
@@ -77,15 +107,18 @@ PreservedAnalyses LoopWalk::run(Loop &L, LoopAnalysisManager &AM, LoopStandardAn
   L.getUniqueExitBlocks(exits);
 
   outs() << "\n\n";
-  outs() << "Number of exits: " << exits.size() << " \n";
+  outs() << "NUMBER OF SUCCESSOR BLOCKS: " << exits.size() << " \n";
+  outs() << "SUCCESSOR BLOCKS: \n";
   for (uint i = 0; i < exits.size(); i++) {
     outs() << *exits[i] << "\n";
   }
 
-
+  outs() << "\n\n\n";
+  // Check if instructions are dead
+  
 
   bool allDominators;
-  std::unordered_set<llvm::Instruction*> instructionsToMove{};
+  std::set<llvm::Instruction*> instructionsToMove{};
 
   for (auto &BB : L.blocks()) {
     allDominators = true;
@@ -95,40 +128,15 @@ PreservedAnalyses LoopWalk::run(Loop &L, LoopAnalysisManager &AM, LoopStandardAn
     if (allDominators) {
       for (llvm::Instruction &I : *BB) {
         // Check if I is in the loop invariant set and if it's safe to move
-        if (loopInvariantInstructions.count(&I) && !I.mayHaveSideEffects()) {
+        if (loopInvariantInstructions.count(&I)) {
           instructionsToMove.insert(&I);
-          loopInvariantInstructions.erase(&I);
         }
       }
-    }
-  }
-
-  outs() << "Instructions that still have to be checked: ";
-  for (auto i : loopInvariantInstructions) {
-    outs() << "\n";
-    i->print(outs());
-  }
-
-  outs() << "\n\n\n";
-  // Check if instructions are dead
-  bool isDead;
-  for(auto &instr : loopInvariantInstructions) {
-    isDead = true;
-    //outs() << "Instruction: " << *instr << "\n";
-
-    // If L does not contain an use of the instruction, the use must be after the loop, so the instruction is not dead
-    // IF L contains all the uses of instr, the instruction is dead
-    for(auto &use : instr->uses()) {
-      //outs() << "Use: " << *use.getUser() << "\n";
-      if (llvm::Instruction *instruction = llvm::dyn_cast<llvm::Instruction>(use.getUser())) {
-        //outs() << "Parent: " << *instruction->getParent() << "\n";
-        isDead &= L.contains(instruction->getParent());
-      }
-    }
-
-    if (!isDead) {
-      if (!instructionsToMove.count(instr)) {
-        instructionsToMove.insert(instr);
+    } else {
+      for (llvm::Instruction &I : *BB) {
+        if (checkIfInstructionIsDead(&I, L) && loopInvariantInstructions.count(&I)){
+          instructionsToMove.insert(&I);
+        }
       }
     }
   }
@@ -137,20 +145,16 @@ PreservedAnalyses LoopWalk::run(Loop &L, LoopAnalysisManager &AM, LoopStandardAn
     outs() << "Instruction to move: " << *inst << "\n";
   }
 
-  
+  // Move the instructions to the preheader
+  bool modified = false;
   BasicBlock *prehead = L.getLoopPreheader();
   for (llvm::Instruction *I : instructionsToMove) {
     I->moveBefore(prehead->getTerminator());
+    modified = true;
   }
   
-
-
-  /*
-  • Sono loop invariant
-  • Si trovano in blocchi che dominano tutte le uscite del loop
-  • Assegnano un valore a variabili non assegnate altrove nel loop
-  • Si trovano in blocchi che dominano tutti i blocchi nel loop che usano lavariabile a cui si sta assegnando un valore*/
-
-
-  return PreservedAnalyses::none();
+  if (modified) {
+    return PreservedAnalyses::none();
+  }
+  return PreservedAnalyses::all();
 }
