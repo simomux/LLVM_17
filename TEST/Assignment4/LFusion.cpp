d8cd6e4c586a88abbaca7b5bb8547f60ff226492
SIMONE MUSSINI <284900@studenti.unimore.it>
2024-06-01 16:31:24 +0200

    Optimized code structure.
    Implemented first fusion step: Replace uses of IV in L2 with IV in L1.
    
    Still need to reararrange CFG for fusion.
    
    Co-authored-by: paridestomeo <paridestomeo@users.noreply.github.com>

--- TEST/Assignment4/LFusion.cpp
+++ TEST/Assignment4/LFusion.cpp
@@ -6,13 +6,16 @@
 #include "llvm/Analysis/DependenceAnalysis.h"
 #include "llvm/IR/TypedPointerType.h"
 
+// Just a helper function to print the pair of loops that are adjacent
 void foundPair(llvm::Loop* &L1, llvm::Loop* &L2, std::set<std::pair<llvm::Loop*, llvm::Loop*>> &set) {
   L1->print(llvm::outs());
   L2->print(llvm::outs());
   set.insert(std::make_pair(L1, L2));
 }
 
-void findAdjacentLoops(llvm::LoopInfo &LI, std::set<std::pair<llvm::Loop*, llvm::Loop*>> &adjacentLoops) {
+
+// First condition: check if loops are adjacent
+void findAdjacentLoops(std::set<std::pair<llvm::Loop*, llvm::Loop*>> &adjacentLoops, llvm::LoopInfo &LI) {
   for (auto *L1 : LI) {
     for (auto *L2: LI) {
       if (L1->isGuarded() && L2->isGuarded()) {
@@ -30,121 +33,173 @@ void findAdjacentLoops(llvm::LoopInfo &LI, std::set<std::pair<llvm::Loop*, llvm:
   }
 }
 
-llvm::PreservedAnalyses llvm::LFusion::run(Function &F, FunctionAnalysisManager &AM) {
-  llvm::LoopInfo &LI = AM.getResult<llvm::LoopAnalysis>(F);
-  
-  // Sctructure containg pairs of loops that are adjacent
-  std::set<std::pair<llvm::Loop*, llvm::Loop*>> adjacentLoops {};
 
-  // Find adjacent loops
-  findAdjacentLoops(LI, adjacentLoops);
-
-  /* for(auto loop : adjacentLoops) {
-    llvm::outs() << "Pair: \n";
-    loop.first->print(llvm::outs());
-    loop.second->print(llvm::outs());
-  } */
-
-  DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
-  PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
-
-  DT.print(llvm::outs());
-  PDT.print(llvm::outs());
-
-  // Sctructure containg the pairs of loops that are candidates for fusion
-  std::set<std::pair<llvm::Loop*, llvm::Loop*>> areCFEquivalent {};
-  
-  // Check if loops are control flow equivalent
-  for (auto loop : adjacentLoops) {
-    if(loop.first->isGuarded()){
-      if(DT.dominates(loop.first->getLoopGuardBranch()->getParent(), loop.second->getLoopGuardBranch()->getParent()) && PDT.dominates(loop.second->getLoopGuardBranch()->getParent(), loop.first->getLoopGuardBranch()->getParent())){
-        llvm::outs() << "\nLoops are control flow equivalent\n";
-        foundPair(loop.first, loop.second, areCFEquivalent);
-      }
-    } else {
-      if (DT.dominates(loop.first->getHeader(), loop.second->getHeader()) && PDT.dominates(loop.second->getHeader(), loop.first->getHeader())) {
-        llvm::outs() << "\nLoops are control flow equivalent\n";
-        foundPair(loop.first, loop.second, areCFEquivalent);
-      }
+// Second condition: check if loops are control flow equivalent
+bool checkEquivalence(std::pair<llvm::Loop*, llvm::Loop*> loop, llvm::DominatorTree &DT, llvm::PostDominatorTree &PDT) {
+  if(loop.first->isGuarded()){
+    if(DT.dominates(loop.first->getLoopGuardBranch()->getParent(), loop.second->getLoopGuardBranch()->getParent()) && PDT.dominates(loop.second->getLoopGuardBranch()->getParent(), loop.first->getLoopGuardBranch()->getParent())){
+      llvm::outs() << "\nLoops are control flow equivalent\n";
+      return 1;
+    }
+  } else {
+    if (DT.dominates(loop.first->getHeader(), loop.second->getHeader()) && PDT.dominates(loop.second->getHeader(), loop.first->getHeader())) {
+      llvm::outs() << "\nLoops are control flow equivalent\n";
+      return 1;
     }
   }
+  return 0;
+}
 
-  
-  ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
 
-  std::set<std::pair<llvm::Loop*, llvm::Loop*>> haveSameTripCount {};
+// Third condition: check if loops have the same trip count
+bool checkSameTripCount(std::pair<llvm::Loop*, llvm::Loop*> loop, llvm::ScalarEvolution &SE) {
+  auto l1Backedges = SE.getBackedgeTakenCount(loop.first);
+  auto l2Backedges = SE.getBackedgeTakenCount(loop.second);
 
-  // Check if loops have the same trip count
-  for (auto loop : areCFEquivalent) {
-    auto l1Backedges = SE.getBackedgeTakenCount(loop.first);
-    auto l2Backedges = SE.getBackedgeTakenCount(loop.second);
+  if (l1Backedges->getSCEVType() == llvm::SCEVCouldNotCompute().getSCEVType() || l2Backedges->getSCEVType() == llvm::SCEVCouldNotCompute().getSCEVType()) return 0;
 
-    if (l1Backedges->getSCEVType() == llvm::SCEVCouldNotCompute().getSCEVType() || l2Backedges->getSCEVType() == llvm::SCEVCouldNotCompute().getSCEVType()) exit(1);
-
-    /* l1Backedges->print(llvm::outs());
-    llvm::outs() << "\n";
-    l2Backedges->print(llvm::outs()); */
-
-    if (l1Backedges == l2Backedges) {
-      llvm::outs() << "\nLoops have the same backedge taken count\n";
-      foundPair(loop.first, loop.second, haveSameTripCount);
-    }
+  if (l1Backedges == l2Backedges) {
+    llvm::outs() << "\nLoops have the same backedge taken count\n";
+    return 1;
   }
-  
-  llvm::DependenceInfo &DI = AM.getResult<llvm::DependenceAnalysis>(F);
+  return 0;
+}
 
-  std::set<std::pair<llvm::Loop*, llvm::Loop*>> candidateForFusion {};
+
+// Fourth condition: check if loops have negative dependencies
+bool checkNegativeDependencies(std::pair<llvm::Loop*, llvm::Loop*> loop) {
 
   // Set that contains all the instructions that has a matrix as operand used in the other loop too.
-  std::set<llvm::Instruction*> temporarySet {};
+  std::set<llvm::Instruction*> dependantInstructions {};
 
+  for (auto &BB : loop.first->getBlocks()) {
+    for (llvm::Instruction &I : *BB) {
+      // Find instructions that works on arrays and get the array pointer
+      if (I.getOpcode() == llvm::Instruction::GetElementPtr){
+        
+        llvm::outs() << "\nInstruction: " << I << "\n";
+        llvm::outs() << "Use in second loop:\n";
 
-  // Check negative depencence distance
-  for (auto loop : haveSameTripCount) {
-    for (auto &BB : loop.first->getBlocks()) {
-      for (llvm::Instruction &I : *BB) {
-        if (I.getOpcode() == llvm::Instruction::GetElementPtr){
-          llvm::outs() << "\nInstruction: " << I << "\n";
-          
-          auto arrayPointer = I.getOperand(0);
-          auto offset = I.getOperand(1);
-
-          llvm::outs() << "Use in second loop:\n";
-          for (auto &use : arrayPointer->uses()) {
-            if (auto inst = llvm::dyn_cast<llvm::Instruction>(use.getUser())) {
-              if (loop.second->contains(inst)) {
-                llvm::outs() << *inst << "\n";
-                if (auto sext = llvm::dyn_cast<llvm::Instruction>(inst->getOperand(1))) {
-                  if (auto phyInstruction = llvm::dyn_cast<llvm::Instruction>(sext->getOperand(0))) {
-                    if (phyInstruction->getOpcode() != llvm::Instruction::PHI) {
-                      temporarySet.insert(phyInstruction);
-                    } else {
-                      ;
-                      ///TODO: Check if the non phi instruction is using a negative offset, in this case you should still be able to merge the loops.
-                    }
+        // Check if pointer is used in the second loop
+        for (auto &use : I.getOperand(0)->uses()) {
+          if (auto inst = llvm::dyn_cast<llvm::Instruction>(use.getUser())) {
+            if (loop.second->contains(inst)) {
+              llvm::outs() << *inst << "\n";
+              // Check if the instruction where the pointer is used, uses a sext instruction. This instuction should be another GetElementPtr in L2
+              if (auto sext = llvm::dyn_cast<llvm::Instruction>(inst->getOperand(1))) {
+                // Check if the sext instruction uses a PHI instruction, if it doesn't it means that there is another instruction that alters the value of the offset
+                if (auto phyInstruction = llvm::dyn_cast<llvm::Instruction>(sext->getOperand(0))) {
+                  switch(phyInstruction->getOpcode()) {
+                    case llvm::Instruction::PHI:
+                    case llvm::Instruction::Sub: // If the non-phi instruction is a sub, it means I'm working with a negative offset, in this case you should still be able to merge the loops.
+                      break;
+                    default:
+                      dependantInstructions.insert(phyInstruction); // Otherwise, the offset is being modified by another instruction, so the loops cannot be merged
                   }
                 }
               }
             }
           }
-        } 
-      }
+        }
+      } 
     }
   }
 
-  ///TODO: Code below this comment must me moved inside del for loop at line 103
-
-  // If there are instructions that violates negative depencence distance, print them and don't fuse
-  if (!temporarySet.empty()) {
+  // Print the instructions that violates the negative dependence distance, if there are any, else return 1
+  if (!dependantInstructions.empty()) {
     llvm::outs() << "\n\nThe loop cannot be fused because the following instructions violates negative depencence distance:\n";
-    for (auto inst : temporarySet) {
+    for (auto inst : dependantInstructions) {
       llvm::outs() << "Instruction: " << *inst << "\n";
     }
-    return llvm::PreservedAnalyses::all();
+    return 0;
+  }
+  return 1;
+}
+
+// Fusion of the 2 loops
+void loopFusion(llvm::Loop* &L1, llvm::Loop* &L2){
+  // Replace uses of IV in L2 with IV in L1
+  
+  auto firstLoopIV = L1->getCanonicalInductionVariable();
+  auto secondLoopIV = L2->getCanonicalInductionVariable();
+
+  llvm::outs() << "First loop IV: " << *firstLoopIV << "\n";
+  llvm::outs() << "Second loop IV: " << *secondLoopIV << "\n";
+
+  secondLoopIV->replaceAllUsesWith(firstLoopIV);
+
+  ///TODO: code below this is just testing, need to make this work
+
+  // Modify CFG as follows:
+  // header 1 --> L2 exit
+  // body 1 --> body 2
+  // body 2 --> latch 1
+  // header 2 --> latch 2
+  
+  auto header1 = L1->getHeader();
+  auto header2 = L2->getHeader();
+
+  auto latch1 = L1->getLoopLatch();
+  auto latch2 = L2->getLoopLatch();
+
+  llvm::outs() << "Header 1: " << *header1 << "\n";
+  llvm::outs() << "Header 2: " << *header2 << "\n";
+  llvm::outs() << "Latch 1: " << *latch1 << "\n";
+  llvm::outs() << "Latch 2: " << *latch2 << "\n";
+
+  // moving induction var to header1 
+  indvar2->moveBefore(indvar1);
+  indvar1->replaceAllUsesWith(indvar2);
+  indvar1->eraseFromParent();
+
+  // update phi instruction 
+  indvar2->replaceIncomingBlockWith(preheader2, preheader1);
+
+  header2->replaceAllUsesWith(header1);
+
+  auto *successor = header1;
+  while (successor != nullptr && successor->getSingleSuccessor() != latch1) {
+      successor = header1->getSingleSuccessor(); //probabile errore loro, sarebbe meglio successor->getSingleSuccessor() secondo me
   }
 
-  llvm::outs() << "Loops can be fused\n";
-  ///TODO: Code for fusing loops
+  // make header2 the successor of header1
+    auto * instr = successor->getTerminator();
+    auto *br = dyn_cast<BranchInst>(instr);
+    br->setSuccessor(0, header2);
 
-  return llvm::PreservedAnalyses::none();
+}
+
+// Main function
+llvm::PreservedAnalyses llvm::LFusion::run(Function &F, FunctionAnalysisManager &AM) {
+
+  // Analysis results
+  llvm::LoopInfo &LI = AM.getResult<llvm::LoopAnalysis>(F);
+  llvm::DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+  llvm::PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
+  llvm::ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
+
+
+  llvm::DependenceInfo &DI = AM.getResult<llvm::DependenceAnalysis>(F);
+  
+  // Structure containg pairs of loops that are adjacent
+  std::set<std::pair<llvm::Loop*, llvm::Loop*>> adjacentLoops {};
+  
+  findAdjacentLoops(adjacentLoops, LI);
+
+  bool modified = 0;
+
+  
+  for (std::pair<llvm::Loop*, llvm::Loop*> loop : adjacentLoops) {
+    if (!checkEquivalence(loop, DT, PDT)) continue;
+    if (!checkSameTripCount(loop, SE)) continue;
+    if (!checkNegativeDependencies(loop)) continue;
+    
+
+    llvm::outs() << "Loops can be fused\n";
+    loopFusion(loop.first, loop.second);
+
+    modified = 1;
+  }
+  if (modified) return llvm::PreservedAnalyses::none();
+  else return llvm::PreservedAnalyses::all();
 }
